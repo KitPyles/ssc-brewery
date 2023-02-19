37aa07609d9f14d11c10a6d17a8603e5267f04b4
KitPyles <pyles.kit@gmail.com>
2023-02-18 23:23:07 -0600

    Completed Assignment 11

---
+++ src/main/java/guru/sfg/brewery/security/perms/CustomerReadPermission.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.security.perms;
+
+// created by kp on 2/18/2023
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('customer.read')")
+public @interface CustomerReadPermission {
+}
