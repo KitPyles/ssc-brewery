3d1d507b84886be60a4e1202cd5d7b1a65596a85
KitPyles <pyles.kit@gmail.com>
2023-02-19 11:46:58 -0600

    Completed Lesson 79

---
+++ src/main/java/guru/sfg/brewery/security/BeerOrderAuthenticationManager.java
@@ -0,0 +1,23 @@
+package guru.sfg.brewery.security;
+
+// created by kp on 2/19/2023
+
+import guru.sfg.brewery.domain.security.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class BeerOrderAuthenticationManager {
+
+    public Boolean CustomerIdMatches(Authentication authentication, UUID customerId){
+        User authenticatedUser = (User) authentication.getPrincipal();
+
+        log.debug("Auth User Customer ID: " + authenticatedUser.getCustomer().getId() + "Customer ID: " + customerId);
+
+        return authenticatedUser.getCustomer().getId().equals(customerId);
+    }
+}
